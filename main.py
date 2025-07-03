b25b421cd0fae626be9234e324807acaa85281fc
Neriya Malachi <neriamalachi@gmail.com>
2025-07-03 09:14:05 +0300

    add grape

--- main.py
+++ main.py
@@ -1,4 +1,54 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.write("hello world!")
-st.write("hello streamlit!")
+
+movies_data = pd.read_csv('./movies.csv')
+# כותרת לגרף
+st.write("Average Move Budget, Grouped by Genre")
+# חישוב ממוצע על פי זאנר
+avg_budget = movies_data.groupby('genre')['budget'].mean().round()
+# מאפס לו את index ומסדר אותם מחדש
+avg_budget = avg_budget.reset_index()
+
+# מקבל את הערכים שהוא רוצה
+genre = avg_budget['genre']
+avg_bud = avg_budget['budget']
+
+#יוצר כנבס בגודל מסוים בכדי ליצור עליו את הגרף
+fig = plt.figure(figsize=(19,10))
+
+# מיצר את הגרף כולל צבע וערכים
+plt.bar(genre,avg_bud,color='maroon')
+
+# נותן שמות לlable
+plt.xlabel('genre')
+plt.ylabel('budget')
+plt.title('Matplotlib Bar Chart Showing the Average \
+Budget of Movies in Each Genre')
+
+
+
+#מריץ את השרת עם הגרף שיצרנו
+st.pyplot(fig)
+
+# col1, col2 =st.columns(2)
+# col1.write('hello col1')
+# col2.write('hello col2')
+#
+# plt.show()
+
+
+score_rating = movies_data['score'].unique().tolist()
+genre_rating = movies_data['genre'].unique().tolist()
+year_rating = movies_data['year'].unique().tolist()
+
+
+
+
+
+
+
+
+
+
